61de4a9dfa3718b5d2bcf73f735c2a0cfb89883f
semorrissey <semorrissey@wpi.edu>
2024-08-02 15:28:44 -0400

    more deployment changes

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+
+// Serve static files from the Angular app
+app.use(express.static(path.join(__dirname, 'dist', 'budget-app')));
+
+// Handle all other routes by returning the Angular app's index.html file
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'budget-app', 'index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
---
+++ src/polyfills.ts
@@ -0,0 +1,14 @@
+/***************************************************************************************************
+ * Load `core-js` for polyfills and `zone.js` for Angular's change detection
+ */
+
+/* Core-js includes polyfills for ES6 and ES7 */
+import 'core-js/es/array';
+import 'core-js/es/number';
+import 'core-js/es/string';
+import 'core-js/es/promise';
+import 'core-js/es/weak-map';
+import 'core-js/es/reflect';
+
+/* Zone.js is required for Angular */
+import 'zone.js';  // Included with Angular CLI.
